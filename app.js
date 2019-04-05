f65494ded2eaf5ee551d22c179360caf58c8049b
Samantha Kellow <sammii.h@icloud.com>
2019-04-05 09:23:39 +0100

    file update

--- app.js
+++ app.js
@@ -37,7 +37,6 @@ let alienBulletIntervalId
 // const highScoreDisplay = document.querySelector('highScoreDisplay')
 // let alienFireIntervalId
 // an array of objects each object is an array of name and scoreDisplay save as json in local storage, saves it as string, pull it back in json.parse and json.stringfy
-
 // ***** make grid *****
 function makeGrid() {
   for(let i = 0; i < width * width; i++) {
@@ -153,7 +152,6 @@ function moveAliens(dir) {
     aliens.forEach(alien => squares[alien].setAttribute('data-step', currentStep))
   }
   const lastAlien = aliens[aliens.length-1]
-  // console.log(lastRow)
   if(lastAlien > lastRow) {
     clearInterval(alienIntervalId)
     gamePlay = false
@@ -221,7 +219,6 @@ function moveBullet(fireIndex) {
 function startGame() {
   clearGrid()
   gamePlay = true
-  // aliens = [0,1,2,3,4,5,6,7,9,10,11,12,13,14,15,16,18,19,20,21,22,23,24,25]
   aliens = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,15,16,17,18,19,20,21,22,23,24,25,26,27,28,30,31,32,33,34,35,36,37,38,39,40,41,42,43]
   if(!gamePlay) {
     clearInterval(timerIntervalId)
@@ -261,7 +258,6 @@ document.addEventListener('DOMContentLoaded', () => {
   levelDisplay = document.querySelector('.level')
   livesDisplay = document.querySelector('.lives')
   startMessages = document.querySelector('.startMessages')
-  // gameLoad = document.querySelector('.gameLoad')
   level = 0
   if (!gamePlay) {
     startButton.addEventListener('click', startGame)
